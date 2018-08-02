8ea34effe47c5b3c3079407b7b264b42728fbddf
sahitesh <sahiteshkumar.reddypelly@gmail.com>
2018-08-03 02:34:03 +0530

    Instructions on project start and launch are added. Removed all the compiled python files

--- attendanceLedger/README.txt
+++ attendanceLedger/README.txt
@@ -1,1 +1,22 @@
-https://www.sitepoint.com/how-to-install-mysql/
+
+# Project Quick Setup and Start
+
+1. Install Anaconda-3.5.2  by adding environment variables(check the box) during setup
+2. Install VS Code-1.25.1 for IDE.
+3. Install virtualenv 'pip install virtualenv'
+4. Create a project folder 'mkdir Vaagdevi'
+5. Navigate to Project folder ' cd Vaagdevi'
+6. Clone git repo 'git clone https://github.com/sahitesh/student_attendance'
+7. Create a virtual environment for the project 'virtualenv vaagdevi_env'
+8. Activate the environment 'vaagdevi_env\Scripts\activate'
+9. Navigate to student_attendance\attendanceLedger 'cd student_attendance\attendanceLedger'
+10. Install requirements 'pip install -r requirements'
+11. Install the whl file for mysqlclient 'pip install mysqlclient-1.3.13-cp36-cp36m-win_amd64.whl'
+12. Start the server 'python manage.py runserver'
+
+# Project Quick Launch
+
+1. VS Code starts in git repo 'student_attendance'
+2. Go back a directory('cd..') and activate environment 'vaagdevi_env\Scripts\activate'
+3. Navigate to Python project directory 'cd student_attendance\attendanceLedger'
+4. Start the server 'python manage.py' runserver
--- attendanceLedger/visualizations/models.py
+++ attendanceLedger/visualizations/models.py
@@ -27,50 +27,51 @@ class FirstYearStudent(models.Model):
         db_table = 'firstyearsemester1_1718'
 
 class SecondYearStudent(models.Model):
+    hall_ticket_number = models.CharField(max_length = 12, primary_key=True)
     batch_id = models.IntegerField(default = 0)
+    section_id = models.CharField(max_length = 5)
+    department = models.CharField(max_length = 5)
     coa_grade = models.CharField(max_length = 5)
     coa_points = models.IntegerField(default = 0)
     dbms_grade = models.CharField(max_length = 5)
     dbms_points = models.IntegerField(default = 0)
     dbmslab_grade = models.CharField(max_length = 5)
     dbmslab_points = models.IntegerField(default = 0)
-    department = models.CharField(max_length = 5)
     dld_mp_grade = models.CharField(max_length = 5)
     dld_mp_points = models.IntegerField(default = 0)
     dldmplab_grade = models.CharField(max_length = 5)
     dldmplab_points = models.IntegerField(default = 0)
     es_grade = models.CharField(max_length = 5)
     es_points = models.IntegerField(default = 0)
-    hall_ticket_number = models.CharField(max_length = 12, primary_key=True)
     mfcs_grade = models.CharField(max_length = 5)
     mfcs_points = models.IntegerField(default = 0)
     oopjava_grade = models.CharField(max_length = 5)
     oopjava_points = models.IntegerField(default = 0)
     oopjavalab_grade = models.CharField(max_length = 5)
     oopjavalab_points = models.IntegerField(default = 0)
-    section_id = models.CharField(max_length = 5)
     
     class Meta:
         db_table = 'secondyearsemester1_1718'
 
 class ThirdYearStudent(models.Model):
+    hall_ticket_number = models.CharField(max_length = 12, primary_key=True)
+    batch_id = models.IntegerField(default = 0)
+    section_id = models.CharField(max_length = 5)
+    department = models.CharField(max_length = 5)
     adbms_grade = models.CharField(max_length = 5)
     adbms_points = models.IntegerField(default = 0)
-    batch_id = models.IntegerField(default = 0)
     cd_grade = models.CharField(max_length = 5)
     cd_points = models.IntegerField(default = 0)
     cncdlab_grade = models.CharField(max_length = 5)
     cncdlab_points = models.IntegerField(default = 0)
     dccn_grade = models.CharField(max_length = 5)
     dccn_points = models.IntegerField(default = 0)
-    department = models.CharField(max_length = 5)
     dm_grade = models.CharField(max_length = 5)
     dm_points = models.IntegerField(default = 0)
     dmlab_grade = models.CharField(max_length = 5)
     dmlab_points = models.IntegerField(default = 0)
     dwdm_grade = models.CharField(max_length = 5)
     dwdm_points = models.IntegerField(default = 0)
-    hall_ticket_number = models.CharField(max_length = 12, primary_key=True)
     hpve_grade = models.CharField(max_length = 5)
     hpve_points = models.IntegerField(default = 0)
     ipr_grade = models.CharField(max_length = 5)
@@ -79,7 +80,6 @@ class ThirdYearStudent(models.Model):
     ooad_points = models.IntegerField(default = 0)
     se_grade = models.CharField(max_length = 5)
     se_points = models.IntegerField(default = 0)
-    section_id = models.CharField(max_length = 5)
     
     class Meta:
         db_table = 'thirdyearsemester1_1718'
