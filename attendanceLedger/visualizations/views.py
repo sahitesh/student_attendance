ff331db16838ad24e0003c9c8945102a6b706435
sahitesh <sahiteshkumar.reddypelly@gmail.com>
2018-08-04 11:04:01 +0530

    Added all the other tables also to the view.

--- attendanceLedger/visualizations/views.py
+++ attendanceLedger/visualizations/views.py
@@ -2,14 +2,21 @@ from rest_framework.views import APIView
 from django.shortcuts import render
 from django.http import HttpResponse
 from .db import GetTableData
-from .models import FirstYearStudent
+from .models import FirstYearStudent, SecondYearStudent, ThirdYearStudent
+from django.db.models import Avg, Max, Min, Sum
 
 class Home(APIView):
     def get(self, request):
-        header = [a.name for a in FirstYearStudent._meta.get_fields()]
-        header = [a.replace('_',' ').title() for a in header]
+        first_header = [a.name for a in FirstYearStudent._meta.get_fields()]
+        first_header = [a.replace('_',' ').title() for a in first_header]
         
-        return render(request, 'home.html', {'records': FirstYearStudent.objects.all(), 'header':header})
+        second_header = [a.name for a in SecondYearStudent._meta.get_fields()]
+        second_header = [a.replace('_',' ').title() for a in second_header]
+
+        third_header = [a.name for a in ThirdYearStudent._meta.get_fields()]
+        third_header = [a.replace('_',' ').title() for a in third_header]
+
+        return render(request, 'home.html', {'firstYear': FirstYearStudent.objects.all(), 'first_header':first_header, 'secondYear': SecondYearStudent.objects.all(), 'second_header':second_header, 'thirdYear': ThirdYearStudent.objects.all(), 'third_header':third_header})
 
 class GetTable(APIView):
     def get(self, request):
