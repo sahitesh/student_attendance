66074893d872fe6985619c0c82c9ff4bd6a9a13f
sahitesh <sahiteshkumar.reddypelly@gmail.com>
2018-08-04 10:16:43 +0530

    Added Container for Tables

--- attendanceLedger/visualizations/views.py
+++ attendanceLedger/visualizations/views.py
@@ -8,6 +8,7 @@ class Home(APIView):
     def get(self, request):
         header = [a.name for a in FirstYearStudent._meta.get_fields()]
         header = [a.replace('_',' ').title() for a in header]
+        
         return render(request, 'home.html', {'records': FirstYearStudent.objects.all(), 'header':header})
 
 class GetTable(APIView):
